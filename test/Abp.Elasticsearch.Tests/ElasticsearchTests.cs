22bb3ff2d4498f69dbcddd8d3a57bbf36568c077
阿星Plus <hackxing@foxmail.com>
2020-07-10 16:32:38 +0800

    ✅

---
+++ test/Abp.Elasticsearch.Tests/ElasticsearchTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Xunit;
+
+namespace Abp.Elasticsearch.Tests
+{
+    public class ElasticsearchTests
+    {
+        [Fact]
+        public void GetClient()
+        {
+            var service = GetElasticsearchService();
+
+            //service...
+
+            Assert.NotNull(service);
+        }
+
+        private static IElasticsearchService GetElasticsearchService()
+        {
+            var application = AbpApplicationFactory.Create<TestModule>();
+            application.Initialize();
+
+            return application.ServiceProvider.GetService<IElasticsearchService>();
+        }
+    }
+}
--- test/Abp.Elasticsearch.Tests/TestModule.cs
+++ test/Abp.Elasticsearch.Tests/TestModule.cs
@@ -5,5 +5,12 @@ namespace Abp.Elasticsearch.Tests
     [DependsOn(typeof(AbpElasticsearchModule))]
     public class TestModule : AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<ElasticsearchOptions>(options =>
+            {
+                options.Nodes = new System.Collections.Generic.List<string> { "http://127.0.0.1:9200" };
+            });
+        }
     }
 }
